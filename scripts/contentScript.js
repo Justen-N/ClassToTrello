fd1469c42a6a76aa864f8ca0f22242c3eb6060a6
Justen-N <losthalflingrogue@gmail.com>
2020-06-25 20:25:15 -0600

    setup for Canvas OAuth Workaround

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -57,14 +57,8 @@ function init() {
         $("#trello_logged_out").css("display", "initial");
         location.reload();
     });
-    $("#connectCanvas").click(function () {
-
-        let canvas_url = "https://byui.instructure.com/login/oauth2/auth?client_id=" + C_APP_KEY + "&response_type=code&redirect_uri=" + redirect;
-        chrome.identity.launchWebAuthFlow({
-            "url": canvas_url
-        }, function (responseUrl) {
-            console.log(responseUrl);
-        });
+    $("#canvas-access").click(function(){
+        
     });
     $("#trelloTutorial").click(function () {
         if (Trello.token()) {
